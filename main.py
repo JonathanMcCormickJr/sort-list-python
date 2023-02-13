decd5d913591f38f18a235eead0591688e4e082c
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-13 22:59:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+names = ['Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia', 'Mia', 'Charlotte', 'Amelia', 'Harper', 'Evelyn', 'Abigail', 'Emily', 'Elizabeth', 'Avery', 'Sofia', 'Ella', 'Madison', 'Scarlett', 'Victoria', 'Aria', 'Grace', 'Chloe', 'Camila', 'Penelope', 'Riley', 'Nora', 'Lily', 'Eleanor', 'Hazel', 'Aubrey', 'Ruby', 'Adalynn', 'Adalyn', 'Adalene', 'Adaline', 'Adalina', 'Adalisse', 'Adaliz', 'Adalene', 'Adalicia', 'Adalisse', 'Adalita', 'Adalicia', 'Adaliz', 'Adalena', 'Adaleta', 'Adalicia', 'Adalisse', 'Jordan', 'Taylor', 'Ethan', 'Mason', 'Liam', 'Noah', 'Aiden', 'Lucas', 'Elijah', 'Oliver', 'Jaxon', 'Jayden', 'Logan', 'Alexander', 'Caleb', 'Eli', 'Leah', 'Avery', 'Aurora', 'Avery', 'Jordan', 'Avery', 'Jorden', 'Riley', 'Jordanne']
+
+def sort_list(list):
+    list.sort()
+    return list
+
+print(sort_list(names))
